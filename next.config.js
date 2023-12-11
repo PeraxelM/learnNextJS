2ba55f4da9b4ef4a12fa8ec539a65a0573809581
Peraxel Märs <peraxel.mars+github@outlook.com>
2023-12-11 17:29:23 +0100

    First commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-
 module.exports = nextConfig;
+
+// Vad skulle detta behövas för?
+/*const path = require('path')
+ 
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+}
+*/
