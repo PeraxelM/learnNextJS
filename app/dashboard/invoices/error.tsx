3c1844de0f86bb4b02835e52a4cef7e4cf61c295
Peraxel Märs <peraxel.mars+github@outlook.com>
2023-12-16 13:14:36 +0100

    Error message

--- app/dashboard/invoices/error.tsx
+++ app/dashboard/invoices/error.tsx
@@ -14,10 +14,11 @@ export default function Error({
     console.error(error);
   }, [error]);
  
+  const errorMessage = error.message;
   return (
     <main className="flex h-full flex-col items-center justify-center">
       <h2 className="text-center">Something went wrong!</h2>
-      <p>{error.message}</p>
+      <p>{errorMessage}</p>
       <button
         className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
         onClick={
